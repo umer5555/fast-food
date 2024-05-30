974298b86e1f4f586df5c1310c68afa0378684a7
Muhammad Umar <39667413+umer5555@users.noreply.github.com>
2024-05-30 19:51:54 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import MainHeader from './components/MainHeader';
-import { useEffect, useState } from 'react';
+
 
 
 function App() {
-
-  const [data,setData]=useState([])
-
-  useEffect(()=>{
-    fetch("https://services6.arcgis.com/qOD6P9mFnaiJRBZ0/ArcGIS/rest/services/HEA_Diabetes/FeatureServer/0/query?where=1=1&outFields=*&returnGeometry=false&f=json")
-    .then((response)=>{
-      response.json().then((res)=>{
-        setData(res)
-        console.log(res)
-      })
-    })
-  })
+ 
 
   return (
+    
    <>
    
       <MainHeader />
-      <h1 className="text-3xl font-bold underline" >
-     Hello Worldaa
-</h1>
+ 
 
    </>
+  
   );
 }
 
